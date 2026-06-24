329696a05aedf06d732a921bcb89f5c8ab17d9b1
anthonyP1202 <144995617+anthonyP1202@users.noreply.github.com>
2026-06-24 23:36:41 +0200

    redid the setting with version 34

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout() {
 
   useEffect(() => {
     SecureStore.getItemAsync('auth_token').then((token) => {
-      if (!token) router.replace('/(auth)/login');
+      if (!token) router.replace('/login');
       setChecked(true);
     });
   }, []);
