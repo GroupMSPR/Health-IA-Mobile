9194c75de22c64b4f8a93cc40eb9a44c9d121ecc
anthonyP1202 <144995617+anthonyP1202@users.noreply.github.com>
2026-06-25 00:38:07 +0200

    added visual loading and default load for _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Slot, useRouter } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { View, Text } from 'react-native';
 
 const queryClient = new QueryClient();
 
@@ -11,12 +12,18 @@ export default function RootLayout() {
 
   useEffect(() => {
     SecureStore.getItemAsync('auth_token').then((token) => {
-      if (!token) router.replace('/login');
+      if (!token) router.replace('/(auth)/login');
       setChecked(true);
     });
   }, []);
 
-  if (!checked) return null;
+  if (!checked) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Loading app...</Text>
+      </View>
+    );
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
