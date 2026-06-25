80a1446a31da16e094706e2a7c4d361e36edddbd
anthonyP1202 <144995617+anthonyP1202@users.noreply.github.com>
2026-06-25 22:42:00 +0200

    added basic export for all file

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,6 +1,6 @@
 import { View } from "react-native"
 
-const Profile = () => {
+const Create = () => {
     
     return (
         <View style={{ flex:1, flexDirection: 'row' }}>
@@ -14,4 +14,4 @@ const Profile = () => {
     )
 }
 
-export default Profile
+export default Create
