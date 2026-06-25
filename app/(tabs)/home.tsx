c8a142a4c30cb033259d3f835c0552059317371c
anthonyP1202 <144995617+anthonyP1202@users.noreply.github.com>
2026-06-25 22:42:00 +0200

    added basic export for all file

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,16 @@
+import { View } from "react-native"
+
 const Home = () => {
     
     return (
-        <div>
-            
-        </div>
+        <View style={{ flex:1, flexDirection: 'row' }}>
+            <View style={{ flex:2, backgroundColor: 'red' }}>
+
+            </View>
+            <View style={{ flex:8, backgroundColor: 'green' }}>
+
+            </View>
+        </View>
     )
 }
 
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,17 @@
+import { View } from "react-native"
+
+const Profile = () => {
+    
+    return (
+        <View style={{ flex:1, flexDirection: 'row' }}>
+            <View style={{ flex:2, backgroundColor: 'red' }}>
+
+            </View>
+            <View style={{ flex:8, backgroundColor: 'green' }}>
+
+            </View>
+        </View>
+    )
+}
+
+export default Profile
