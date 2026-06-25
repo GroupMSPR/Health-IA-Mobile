840275daa56e12f80750182e888bcfa6249b2ef3
anthonyP1202 <144995617+anthonyP1202@users.noreply.github.com>
2026-06-25 22:42:23 +0200

    moved token logic to index

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from 'react';
+import { Redirect, Slot, useRouter } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { View, Text } from 'react-native';
 
-export default function Index() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>App is running 🚀</Text>
-    </View>
-  );
+const queryClient = new QueryClient();
+
+export default function RootLayout() {
+  const [token, setToken] = useState<string | null | undefined>(undefined);
+
+  useEffect(() => {
+    SecureStore.getItemAsync('auth_token').then(setToken);
+  }, []);
+
+  if (token === undefined) {
+    return (
+      <View>
+        <Text>loading ...</Text>
+      </View>
+    );
+  }
+
+  if (!token){
+    return <Redirect href="/(tabs)/home"/>;
+  }
+
+  return <Redirect href="/(tabs)/home"/>
 }
