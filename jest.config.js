456611a6523f12dc305fa674f071326b62bb00ed
Ilan ARFI <i.arfipro@outlook.fr>
2026-07-01 01:14:29 +0200

    maj CI

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: "jest-expo",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  collectCoverageFrom: [
+    "app/**/*.{ts,tsx}",
+    "context/**/*.{ts,tsx}",
+    "lib/**/*.{ts,tsx}",
+    "assets/componants/**/*.{ts,tsx}",
+    "!**/_layout.tsx",
+    "!**/node_modules/**",
+  ],
+  coverageReporters: ["text", "text-summary", "lcov"],
 };
